8a3812facc6396b2709f2f5d6ca1599af0fea05e
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-06-22 22:11:35 +0700

    22-6-2025

---
+++ src/main/java/banganas/spring/belajar_spring_validation/BelajarSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package banganas.spring.belajar_spring_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/banganas/spring/belajar_spring_validation/BelajarSpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package banganas.spring.belajar_spring_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
